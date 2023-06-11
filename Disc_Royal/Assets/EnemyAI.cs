80c2900dc97d7953177ee8174f4b351102ed5179
H4zard101 <Harry2.Hodgkins@live.uwe.ac.uk>
2023-06-11 23:09:27 +0100

    created a FSM and added a nav mesh, currently a few issues with it and the camera stutters

---
+++ Disc_Royal/Assets/EnemyAI.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask WhatIsGround;
+    public LayerMask whatIsPlayer;
+
+    // Patroling
+    public Vector3 walkpoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    // Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    // States
+    public float sightRange;
+    public float attackRange;
+    public bool playerInSightRange;
+    public bool playerInAttackRange;
+
+    public void Awake()
+    {
+        player = GameObject.Find("FirstPersonPlayer").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if(!playerInSightRange && !playerInAttackRange)
+        {
+            Patroling();
+        }
+
+        if(playerInSightRange && !playerInAttackRange)
+        {
+            Chase();
+        }
+
+        if (playerInSightRange && playerInAttackRange)
+        {
+            Attack();
+        }
+    }
+
+
+    public void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange - 10.0f, walkPointRange + 10.0f);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkpoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        if (Physics.Raycast(walkpoint, -transform.up, 2f, WhatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+    public void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+
+
+    // States the AI can be in
+    private void Patroling()
+    {
+        if(!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+
+        if(walkPointSet)
+        {
+            agent.SetDestination(walkpoint);
+        }
+        
+        Vector3 distanceToWalkPoint = transform.position - walkpoint;
+        
+        if(distanceToWalkPoint.magnitude <1f)
+        {
+            walkPointSet = false;
+        }
+    }
+    private void Chase()
+    {
+        agent.SetDestination(player.position);
+    }
+    private void Attack()
+    {
+        agent.SetDestination(transform.position);
+        //transform.LookAt(player);
+
+        if(!alreadyAttacked)
+        {
+            Debug.Log("attacking");
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+}
